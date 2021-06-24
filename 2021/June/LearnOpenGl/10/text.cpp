aacf8675008841627db0fedb0048977bbbab7e3b
xuxiuquan@kaochong.com <xuxiuquan@kaochong.com>
2021-06-24 15:40:16 +0800

    add: render text

---
+++ 2021/June/LearnOpenGl/10/text.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: xiuquanxu
+ * @Company: kaochong
+ * @Date: 2021-06-24 13:58:49
+ * @LastEditors: xiuquanxu
+ * @LastEditTime: 2021-06-24 13:59:04
+ */
+#include <iostream>
+#include <map>
+#include <string>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
